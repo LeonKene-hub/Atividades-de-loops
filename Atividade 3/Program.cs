d3e50db35498e27067cdbe65c7bfe4acf7d4ac10
Pedro H. Alves <78762616+LeonKene-hub@users.noreply.github.com>
2023-04-10 09:11:20 -0300

    Update Program.cs

--- Atividade 3/Program.cs
+++ Atividade 3/Program.cs
@@ -1,7 +1,6 @@
 ﻿//entrada
 Console.WriteLine($"Informe o numero que deseja usar para formar a tabuada:");
 int numero = int.Parse(Console.ReadLine());
-
 int resultado = 0;
 //processo
 for (int vezes = 1; vezes <= 10; vezes++)
@@ -9,5 +8,3 @@ for (int vezes = 1; vezes <= 10; vezes++)
     resultado = numero * vezes;
     Console.WriteLine($"{numero} * {vezes} = {resultado}");
 }
-
-//saida
